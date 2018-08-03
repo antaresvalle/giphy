87232905b3be5495462d5f9ecff383538078bbd1
antaresvalle <antaresvll561@gmail.com>
2018-08-03 11:31:38 -0500

    first commit: se agrega funcionalidad y algo de estilo

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(() => {
+    const dibujarGifs = (data) => {
+        let gif = "";
+        let url = "";
+        data.forEach((element) =>{
+            gif = element.images.downsized_large.url;
+            url = element.bitly_gif_url;
+            $("#elementos").append(armarTemplate(gif, url));
+        });
+    }
+
+    const armarTemplate = (gif,url) => {
+        console.log('hola img');
+        let t = `<div class='elemento'><img src='${gif}'/><a href='${url}'>Ver mas</a></div>`;
+        console.log(t);
+        return t;
+    }
+
+    const ajaxGif = (gif) => {
+        $.ajax({
+            url: 'http://api.giphy.com/v1/gifs/search',
+            type: 'GET',
+            datatype: 'json',
+            data: {
+                q: gif,
+                api_key: 'hvI9VlayLdibEu7J75wDGmC16Z896Ttk'
+            }
+        })
+        .done((response) => {
+            console.log(response);
+            dibujarGifs(response.data);
+        })
+        .fail(() => {
+            console.log('error');
+        });
+    }
+
+    $("#buscar-gif").click((event) => {
+        console.log("entro");
+        $("#elementos").empty();
+        let gif = $("#gif-text").val();
+        ajaxGif(gif);
+    })
+})
